e5c5f6ef1fbcdb9b5f084470090298c94833cfed
alexhetley6107 <hetleyofficial@gmail.com>
2024-02-21 09:10:10 +0200

    feat: resize element

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -13,13 +13,19 @@ function createCol(col) {
   return `
     <div class="column">
       ${col}
+      <div class="col-resize"></div>
     </div>
   `;
 }
 function createRow(index, content) {
+  const resize = index ? '<div class="row-resize"></div>' : '';
+
   return `
     <div class="row">
-      <div class="row-info">${index ?? ''}</div>
+      <div class="row-info">
+        ${index ?? ''}
+        ${resize}
+      </div>
       <div class="row-data">${content}</div>
     </div>
   `;
@@ -35,7 +41,7 @@ export function createTable(rowsCount = 20) {
 
   const cols = new Array(colsCount).fill('').map(toChar).map(createCol).join('');
 
-  rows.push(createRow(cols));
+  rows.push(createRow(null, cols));
 
   for (let i = 0; i < rowsCount; i++) {
     const rowContent = new Array(colsCount).fill('').map(createCell).join('');
