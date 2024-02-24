08dd5a0f032c396289d1bcdbc70a2f44972b8daf
alexhetley6107 <hetleyofficial@gmail.com>
2024-02-24 14:01:43 +0200

    feat: resize optimization

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -21,26 +21,36 @@ export class Table extends ExcelComponent {
       const $parent = $resizer.closest('[data-type="resizable"]');
       const coords = $parent.getCoords();
       const type = $resizer.data.resize;
+      const sideProp = type === 'col' ? 'bottom' : 'right';
+      let value;
 
-      const cells = this.$root.findAll(`[data-col="${$parent.data.col}"]`);
+      $resizer.css({ opacity: 1, [sideProp]: '-5000px' });
 
       document.onmousemove = (event) => {
         if (type === 'col') {
-          const value = coords.width + event.pageX - coords.right;
-
-          $parent.css({ width: value + 'px' });
-          cells.forEach((el) => (el.style.width = value + 'px'));
+          const delta = event.pageX - coords.right;
+          value = coords.width + delta;
+          $resizer.css({ right: -delta + 'px' });
         } else {
-          const value = coords.height + event.pageY - coords.bottom;
-
-          $parent.css({ height: value + 'px' });
-
-          cells.forEach((el) => (el.style.height = value + 'px'));
+          const delta = event.pageY - coords.bottom;
+          value = coords.height + delta;
+          $resizer.css({ bottom: -delta + 'px' });
         }
       };
 
       document.onmouseup = () => {
         document.onmousemove = null;
+        document.onmouseup = null;
+
+        if (type === 'col') {
+          $parent.css({ width: value + 'px' });
+          this.$root.findAll(`[data-col="${$parent.data.col}"]`).forEach((el) => (el.style.width = value + 'px'));
+        } else {
+          $parent.css({ height: value + 'px' });
+          this.$root.findAll(`[data-row="${$parent.data.row}"]`).forEach((el) => (el.style.height = value + 'px'));
+        }
+
+        $resizer.css({ opacity: 0, bottom: 0, right: 0 });
       };
     }
   }
