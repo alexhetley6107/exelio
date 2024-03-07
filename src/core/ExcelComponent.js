39e142710f9adde2e2deb4b18ba72775b7e745d5
alexhetley6107 <hetleyofficial@gmail.com>
2024-03-07 17:34:30 +0200

    feat: unsubscribing

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -5,6 +5,8 @@ export class ExcelComponent extends DomListener {
     super($root, options.listeners);
     this.name = options.name || '';
     this.emitter = options.emitter;
+    this.unsubscribers = [];
+
     this.prepare();
   }
 
@@ -18,7 +20,17 @@ export class ExcelComponent extends DomListener {
     this.initDOMListeners();
   }
 
+  $emit(event, ...args) {
+    this.emitter.emit(event, ...args);
+  }
+
+  $on(event, fn) {
+    const unsub = this.emitter.subscribe(event, fn);
+    this.unsubscribers.push(unsub);
+  }
+
   destroy() {
     this.removeDOMListeners();
+    this.unsubscribers.forEach((unsub) => unsub());
   }
 }
