1e80e530deb5fa3cd87a31e1477e158289de6c5b
alexhetley6107 <hetleyofficial@gmail.com>
2024-03-07 19:12:27 +0200

    feat: state structure

--- src/index.js
+++ src/index.js
@@ -4,9 +4,14 @@ import { Header } from '@/components/header/Header';
 import { Toolbar } from '@/components/toolbar/Toolbar';
 import { Formula } from '@/components/formula/Formula';
 import { Table } from '@/components/table/Table';
+import { rootReducer } from '@/state/rootReducer';
+import { createStore } from '@/state/createStore';
+
+const store = createStore(rootReducer);
 
 const excel = new Excel('#app', {
   components: [Header, Toolbar, Formula, Table],
+  store,
 });
 
 excel.render();
