7667d4d3238dab8872a4dc89ab3242f04fd62569
alexhetley6107 <hetleyofficial@gmail.com>
2024-03-17 16:29:10 +0200

    feat: header btns

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,6 +3,7 @@ import { Dom } from '@/core/Dom';
 import { defaultTitle } from '@/constants';
 import * as actions from '@/state/actions';
 import { debounce } from '../../core/utils';
+import { ActiveRoute } from '../../core/routing/ActiveRoute';
 
 export class Header extends ExcelComponent {
   static className = 'excel__header';
@@ -10,7 +11,7 @@ export class Header extends ExcelComponent {
   constructor($root, options) {
     super($root, {
       name: 'Header',
-      listeners: ['input'],
+      listeners: ['input', 'click'],
       ...options,
     });
   }
@@ -25,12 +26,12 @@ export class Header extends ExcelComponent {
     return `
      <input type="text" class="input" value="${title}" />
       <div>
-        <div class="button">
-          <i class="material-icons">delete</i>
+        <div class="button" data-button="remove">
+          <i class="material-icons" data-button="remove">delete</i>
         </div>
 
-        <div class="button">
-          <i class="material-icons">exit_to_app</i>
+        <div class="button" data-button="exit">
+          <i class="material-icons" data-button="exit">exit_to_app</i>
         </div>
       </div>
     `;
@@ -39,5 +40,21 @@ export class Header extends ExcelComponent {
   onInput(e) {
     const $target = new Dom(e.target);
     this.$dispatch(actions.changeTitle($target.text()));
+  }
+
+  onClick(e) {
+    const $target = new Dom(e.target);
+
+    if ($target.data.button === 'remove') {
+      const decision = confirm('Вы действительно хотите удалить таблицу?');
+      if (decision) {
+        localStorage.removeItem('excel:' + ActiveRoute.param);
+        ActiveRoute.navigate('');
+      }
+    }
+
+    if ($target.data.button === 'exit') {
+      ActiveRoute.navigate('');
+    }
   }
 }
